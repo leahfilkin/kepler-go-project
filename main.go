70ac3b7806f1951b93b6f30440c16c14115e99ab
leahfilkin-myob <leah.filkin@myob.com>
2022-05-13 16:20:02 +1200

    adds header and meta data handlers

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+type Metadata struct {
+	Version       string `json:"version"`
+	Description   string `json:"description"`
+	LastCommitSha string `json:"last_commit_sha"`
+}
+
 func hello(w http.ResponseWriter, req *http.Request) {
 	// return content type, text plain - header
+	w.Header().Add("Content-Type", `text/plain`)
 	fmt.Fprintf(w, "hello world\n")
 }
 
@@ -22,11 +30,28 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func metadata(w http.ResponseWriter, req *http.Request) {
+	myapplication := make(map[string][]Metadata)
+	metadata := Metadata{
+		Version:       "1.0",
+		Description:   "pre-interview technical test",
+		LastCommitSha: "abc57858585",
+	}
+	myapplication["myapplication"] = []Metadata{metadata}
+	//	myapplication["myapplication"] = append(myapplication["myapplication"], metadata)
+	enc := json.NewEncoder(w)
+	w.Header().Add("Content-Type", `application/json`)
+	enc.SetIndent("", "    ")
+	if err := enc.Encode(myapplication); err != nil {
+		fmt.Fprintf(w, "Error: %s", err)
+	}
+}
 func main() {
 
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/health", health)
 	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/metadata", metadata)
 
 	http.ListenAndServe(":8090", nil)
 }
